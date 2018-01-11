a737c95226cf036bc82894332aaf97dacd824767
MrZJoker <280588121@qq.com>
2018-01-11 17:28:29 +0800

    下拉框

---
+++ FaceState/FaceState/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FaceState
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(655, 563);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(80, 80);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(50, 50);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(283, 521);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(102, 46);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(647, 42);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(919, 46);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(38, 632);
+            this.vScrollBar1.TabIndex = 3;
+            this.vScrollBar1.Value = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 687);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "寝室情况";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ FaceState/FaceState/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FaceState
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            asd(0, 0,new List<string>() { "644","张熙贤","吴宗阳","张进涛"});
+           
+
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param 索引坐标="indexX"></param>
+        /// <param 索引坐标="indexY"></param>
+        /// <param 有几个人在="Nub"></param>
+        /// <param 人员信息="information">寝室号，在寝姓名1，在寝姓名2，在寝姓名3，在寝姓名四</param>
+        public void asd(int indexX,int indexY,List<string> information)
+        {
+
+            foreach (var item in DropItem(information,indexX,indexY))
+            {
+                this.Controls.Add(item);
+            }
+
+
+        }
+
+        public List<Label> DropItem( List<string> information, int indexX, int indexY)
+        {
+            System.Drawing.Image ac = System.Drawing.Image.FromFile(@"C:\Users\Administrator\Desktop\PNG图片\2.png");
+            Graphics my = Graphics.FromHwnd(this.Handle);
+            imageList1.Images.Add(ac);
+            System.Drawing.Image b = System.Drawing.Image.FromFile(@"C:\Users\Administrator\Desktop\PNG图片\blue_business_06.png");
+            imageList2.Images.Add(b);
+            List<Label> D = new List<Label>();
+
+            int x = indexX * 200 + 70;
+            int y = indexY * 200 + 70;
+            imageList1.Draw(my, new Point(x, y), 0);
+           
+            for (int i = 0; i < (information.Count); i++)
+            {
+                D.Add(new Label());
+                System.Diagnostics.Debug.Print(i + "");
+                D[i].Text = information[i];
+                D[i].Width = 50;
+                switch (i)
+                {
+                    case 0:
+                        D[i].Font = new Font("宋体", 12);
+                        D[i].Location = new Point(x + 20, y + 30);
+                        break;
+                    case 1:
+                        D[i].Location = new Point(x - 50, y + 50);
+
+                        imageList2.Draw(my, new Point(x - 50, y), 0);
+                        break;
+                    case 2:
+                       
+
+                        D[i].Location = new Point(x + 50, y - 50);
+                        imageList2.Draw(my, new Point(x + 10, y - 50), 0);
+                        break;
+                    case 3:
+                      
+                        D[i].Location = new Point(x + 80, y + 50);
+                        imageList2.Draw(my, new Point(x + 80, y), 0);
+                        break;
+                    case 4:
+
+                        D[i].Location = new Point(x + 50, y + 80);
+                        imageList2.Draw(my, new Point(x + 10, y + 80), 0);
+                        break;
+                }
+            }
+            return D;
+   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Image ac = System.Drawing.Image.FromFile(@"C:\Users\Administrator\Desktop\PNG图片\2.png");
+            Graphics my = Graphics.FromHwnd(this.Handle);
+            imageList1.Images.Add(ac);
+            System.Drawing.Image b = System.Drawing.Image.FromFile(@"C:\Users\Administrator\Desktop\PNG图片\blue_business_06.png");
+            imageList2.Images.Add(b);
+            listView1.LargeImageList.Images.Add(ac);
+            listView1.LargeImageList.Draw(my, new Point(0, 0), 0);
+            Label a = new Label();
+            a.Text = "dasdqa";
+            listView1.Controls.Add(a); listView1.Controls.Add(new VScrollBar());
+            ImageList adsa = new ImageList();
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
